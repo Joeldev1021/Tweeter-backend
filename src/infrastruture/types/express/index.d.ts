43bb1a088240a127feedc600b3deb65723311bbb
joeldev <joelpimentel1995@gmail.com>
2022-10-12 11:02:46 -0500

    fix: compile types custom

--- src/infrastruture/types/express/index.d.ts
+++ src/infrastruture/types/express/index.d.ts
@@ -3,10 +3,10 @@ import { IUser } from "../../interface/user.interface";
 // to make the file a module and avoid the TypeScript error
 export { }
 
-declare global {
+declare module 'express-serve-static-core' {
     namespace Express {
         export interface Request {
-            user?: IUser;
+            user?: IUser
         }
     }
 }
