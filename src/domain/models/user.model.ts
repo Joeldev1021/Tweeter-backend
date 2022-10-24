166708818c5176a25eca3f78fcad6daf25ebf850
joeldev <joelpimentel1995@gmail.com>
2022-10-24 10:51:36 -0500

    fix: comment userModel

--- src/domain/models/user.model.ts
+++ src/domain/models/user.model.ts
@@ -1,10 +1,18 @@
-import { EmailVO } from "../value-objects/emial.vo";
-import { PasswordVO } from "../value-objects/password.vo";
-import { UsernameVO } from "../value-objects/username.vo";
+import { EmailVO } from "../value-objects/user/email.vo";
+import { PasswordVO } from "../value-objects/user/password.vo";
+import { UsernameVO } from "../value-objects/user/username.vo";
 import { UuidVO } from "../value-objects/uuid.vo"
 
 export class UserModel {
 
+    /**
+     * The constructor function is a public function that takes in an id, username, email, and password,
+     * and sets them to the class properties
+     * @param {UuidVO} id - UuidVO
+     * @param {UsernameVO} username - UsernameVO
+     * @param {EmailVO} email - EmailVO
+     * @param {PasswordVO} password - PasswordVO
+     */
     constructor(
         public readonly id: UuidVO,
         public username: UsernameVO,
@@ -12,7 +20,12 @@ export class UserModel {
         public password: PasswordVO,
     ) { }
 
-    static createUser() {
-
+    static createUser(
+        id: UuidVO,
+        name: UsernameVO,
+        email: EmailVO,
+        password: PasswordVO
+    ) {
+        return new UserModel(id, name, email, password)
     }
 }
