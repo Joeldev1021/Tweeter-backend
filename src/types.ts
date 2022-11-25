97dd402f6793470599b8f403f4b61a960ade8547
joeldev <joelpimentel1995@gmail.com>
2022-11-25 11:28:39 -0500

    style eslint

--- src/types.ts
+++ src/types.ts
@@ -1,50 +1,47 @@
-
 const UseCases = {
-    UserRegisterUseCase: Symbol.for("UserRegisterUseCase"),
-    UserLoginUseCase: Symbol.for("UserLoginUseCase"),
-    UserProfileUseCase: Symbol.for("UserProfileUseCase"),
-    ProfileFindByQueryFilterUseCase: Symbol.for("ProfileFindByQueryFilterUseCase"),
+    UserRegisterUseCase: Symbol.for('UserRegisterUseCase'),
+    UserLoginUseCase: Symbol.for('UserLoginUseCase'),
+    UserProfileUseCase: Symbol.for('UserProfileUseCase'),
+    ProfileFindByQueryFilterUseCase: Symbol.for(
+        'ProfileFindByQueryFilterUseCase'
+    ),
 
-    TweetCreateUseCase: Symbol.for("TweetCreateUseCase"),
-    TweetFindAllUseCase: Symbol.for("TweetFindAllUseCase"),
-    TweetFindByIdUseCase: Symbol.for("TweetFindByIdUseCase"),
-    TweetFindByOwnerIdUseCase: Symbol.for("TweetFindByOwnerIdUseCase"),
-    TweetUpdateByIdUseCase: Symbol.for("TweetUpdateByIdUseCase"),
-    TweetDeleteByIdUseCase: Symbol.for("TweetDeleteByIdUseCase"),
-    TweetLikeUseCase: Symbol.for("TweetLikeUseCase"),
+    TweetCreateUseCase: Symbol.for('TweetCreateUseCase'),
+    TweetFindAllUseCase: Symbol.for('TweetFindAllUseCase'),
+    TweetFindByIdUseCase: Symbol.for('TweetFindByIdUseCase'),
+    TweetFindByOwnerIdUseCase: Symbol.for('TweetFindByOwnerIdUseCase'),
+    TweetUpdateByIdUseCase: Symbol.for('TweetUpdateByIdUseCase'),
+    TweetDeleteByIdUseCase: Symbol.for('TweetDeleteByIdUseCase'),
+    TweetLikeUseCase: Symbol.for('TweetLikeUseCase'),
 
-    ReplyCreateUseCase: Symbol.for("ReplyCreateUseCase"),
-    ReplyFindAllUseCase: Symbol.for("ReplyFindAllUseCase"),
-    ReplyFindByIdUseCase: Symbol.for("ReplyFindByIdUseCase"),
-    ReplyFindByTweetIdUseCase: Symbol.for("ReplyFindByTweetIdUseCase"),
-    ReplyDeleteByIdUseCase: Symbol.for("ReplyDeleteByIdUseCase"),
-    ReplyFindByOwnerIdUseCase: Symbol.for("ReplyFindByOwnerIdUseCase"),
-    ReplyLikeUseCase: Symbol.for("ReplyLikeUseCase"),
-
-}
+    ReplyCreateUseCase: Symbol.for('ReplyCreateUseCase'),
+    ReplyFindAllUseCase: Symbol.for('ReplyFindAllUseCase'),
+    ReplyFindByIdUseCase: Symbol.for('ReplyFindByIdUseCase'),
+    ReplyFindByTweetIdUseCase: Symbol.for('ReplyFindByTweetIdUseCase'),
+    ReplyDeleteByIdUseCase: Symbol.for('ReplyDeleteByIdUseCase'),
+    ReplyFindByOwnerIdUseCase: Symbol.for('ReplyFindByOwnerIdUseCase'),
+    ReplyLikeUseCase: Symbol.for('ReplyLikeUseCase'),
+};
 
 const Repositories = {
-    UserRepository: Symbol.for("UserRepository"),
-    TweetRepository: Symbol.for("TweetRepository"),
-    ReplyRepository: Symbol.for("ReplyRepository")
-}
+    UserRepository: Symbol.for('UserRepository'),
+    TweetRepository: Symbol.for('TweetRepository'),
+    ReplyRepository: Symbol.for('ReplyRepository'),
+};
 
 const Middlewares = {
-    AuthMiddleware: Symbol.for("AuthMiddleware"),
-}
+    AuthMiddleware: Symbol.for('AuthMiddleware'),
+};
 
 const Services = {
-    JwtService: Symbol.for("JwtService"),
-}
-
+    JwtService: Symbol.for('JwtService'),
+};
 
 const TYPES = {
     ...UseCases,
     ...Repositories,
     ...Middlewares,
-    ...Services
-}
+    ...Services,
+};
 
-export { TYPES }
-
-
+export { TYPES };
